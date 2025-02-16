55e72926d8b4c82db62dc239a1826b64ff83466a
Vladislav19121 <gvlad2322@gmail.com>
2025-02-17 02:04:19 +0300

    добавлена возаожность добавления и удаления товаров в категории, добавлена регистрация аутентификация

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -5,6 +5,13 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.login_view, name = 'login'),
+    path('logout/', views.logout_view, name = 'logout'),
+    path('registration/', views.registration, name='registration'),
+    path('category/<int:id>/', views.open_category, name='category_products'),
+    path('add_product/<int:id>/', views.add_product, name='add_product'),
+    path('delete_product/<int:id>/', views.delete_product, name='delete_product'),
+
 ]
 
 
--- shop_app/views.py
+++ shop_app/views.py
@@ -2,7 +2,67 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import login, authenticate, logout
+from .forms import ProductForm
+from django.contrib.auth.forms import UserCreationForm
 
+def registration(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = UserCreationForm()
+        
+    
+    return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user_log = authenticate(request, username=username, password=password)
+        
+        if user_log is not None:
+            login(request, user_log)  
+            return redirect('home')  
+        else:
+            error_message = "Неверное имя пользователя или пароль."
+            return render(request, 'login.html', {'error': error_message})
+
+    return render(request, 'login.html')    
+            
+def logout_view(request):
+    logout(request)
+    return redirect('home')
 def home(request):
     categories = Category.objects.all()
     return render(request, 'home.html', {'categories': categories})
+
+def open_category(request, id):
+    category = get_object_or_404(Category, id=id)
+    products = category.products.all()
+    return render(request, 'category_products.html', {'products': products, 'category': category})
+
+def add_product(request, id):
+    category = get_object_or_404(Category, id=id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save(commit=False)
+            product.category = category 
+            product.user = request.user
+            product.save()
+            return redirect('category_products', id=category.id)
+    else:
+        form = ProductForm()
+
+    return render(request, 'add_product.html', {'form': form, 'category': category})
+
+def delete_product(request, id):
+    product = get_object_or_404(Product, id=id)
+    if request.user == product.user:
+        product.delete()
+        return redirect('category_products', id = product.category.id)
